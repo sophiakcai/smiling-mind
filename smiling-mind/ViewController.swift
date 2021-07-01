d7c09d50ff3e8fb7ba42d1ca580d4cff6b3e5b9b
IrinaRaleigh <63532291+IrinaRaleigh@users.noreply.github.com>
2021-07-01 10:56:10 -0500

    put info in resource page

--- smiling-mind/ViewController.swift
+++ smiling-mind/ViewController.swift
@@ -49,6 +49,22 @@ class ViewController: UIViewController {
         }
     }
     
+    class ViewController: UIViewController, UITextViewDelegate {
+        @IBOutlet var textView: UITextView!
+
+        override func viewDidLoad() {
+            let attributedString = NSMutableAttributedString(string: "Want to learn iOS? You should visit the best source of free iOS tutorials!")
+            attributedString.addAttribute(.link, value: "https://www.hackingwithswift.com", range: NSRange(location: 19, length: 55))
+
+            textView.attributedText = attributedString
+        }
+
+        func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+            UIApplication.shared.open(URL)
+            return false
+        }
+    }
+    
     
     @IBDesignable class RotatableView: UIView {
 
